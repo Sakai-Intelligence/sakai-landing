cdbbfa257c4daf1550b006be22a04ffdab34c35f
Guillermo <8440725+gortizfigueroa@users.noreply.github.com>
2025-12-22 21:40:10 +0100

    Updated .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
+import Studio from "./pages/Studio";
 import NotFound from "./pages/NotFound";
 import { LocaleProvider } from "@/i18n/LocaleContext";
 
@@ -20,6 +21,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/studio" element={<Studio />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
